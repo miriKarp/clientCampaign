c26e07a833d1b4d05f8ae3c20ac7425c8ee13416
miriKarp <m3136879@gmail.com>
2024-03-08 16:23:16 +0200

    firstCommit

--- src/app/components/images/images.component.ts
+++ src/app/components/images/images.component.ts
@@ -6,26 +6,28 @@ import { interval } from 'rxjs';
   templateUrl: './images.component.html',
   styleUrls: ['./images.component.scss']
 })
-export class ImagesComponent {
+export class ImagesComponent implements OnInit {
 
-  constructor() {
-    interval(6000) // Switch image every minute
-      .subscribe(() => {
-        this.currentImageIndex = (this.currentImageIndex + 1) % this.imageUrls.length;
-        this.showImage = true;
-
-        setTimeout(() => {
-          this.showImage = false;
-        }, 500); // Hide image after 5 seconds
-      });
+  constructor() { }
+  
+  ngOnInit(): void {
+    setInterval(() => {
+      this.showImage = true;
+      this.currentImage = this.imageUrls[Math.floor(Math.random() * this.imageUrls.length)]; // Change image randomly
+      setTimeout(() => {
+        this.showImage = false; // Hide image after 5 seconds
+      }, 500);
+    }, 6000)
   }
 
   imageUrls: string[] = ["../../../assets/logo.JPG", "../../../assets/2.jpg", "../../../assets/3.jpg"];
   currentImageIndex: number = 0;
-  showImage: boolean = true;
+  currentImage: string = '';
+  showImage: boolean = false;
 
   showImages(): void {
 
   }
+
 
 }
